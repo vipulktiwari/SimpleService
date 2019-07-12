0eaa80ab36ac1866cdc63f52b05e1c682969d36a
vipult <vipul.tiwari@druva.com>
2019-07-12 19:25:45 +0530

    simple http server with ping api implementation

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"log"
+	"net/http"
+)
+
+// Our basic service, which will have all the api as interface funtions
+type BasicService interface {
+	Ping(context.Context) (string, error)
+}
+
+type basicService struct{}
+
+func (b basicService) Ping(ctx context.Context) (string, error) {
+	return "pong", nil
+}
+
+// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
+func makePingEndpoint(svc BasicService) endpoint.Endpoint {
+	return func(ctx context.Context, req interface{}) (resp interface{}, err error) {
+		resp, err = svc.Ping(ctx)
+		return resp, err
+	}
+}
+
+func decodePingRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	// as ping is a get request we will do nothing here
+	return nil, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	r := response.(string)
+	_, err := w.Write([]byte(r))
+	return err
+}
+
+func main() {
+	svc := basicService{}
+	pinghandler := httptransport.NewServer(
+		makePingEndpoint(svc),
+		decodePingRequest,
+		encodeResponse,
+	)
+
+	log.Println("server started, listening on port 8080")
+	http.Handle("/ping", pinghandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
